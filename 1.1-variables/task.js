2309166268e8542b609f97d5204fbdc83c4c7ff8
Etnogenes <andrei09.94@mail.ru>
2020-12-09 18:14:13 +0300

    decision

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,26 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let subject = 3;
+	let algebra = a;
+    let geography = g;
+    let physics = p;    
+    let total = (algebra + geography + physics) / subject;
+	return total;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+    let myName = userName;
+    let message = `Привет, мир! Меня зовут ${myName}.`;
+	return message;
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+    let result = x * y + 5 * z + x - 1;
+    return result;
 }
