2c6ef82de5acd540e409cb6058eb745ce2c2a013
Etnogenes <andrei09.94@mail.ru>
2020-12-15 17:04:14 +0300

    First try

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,48 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+    let one = a;
+    let two = b;
+    let three = c;
+    let D = two ** 2 - 4 * one * three;
+    let firstX = (-two + D ** (1 / 2)) / 2 * one;
+    let secondX = (-two - D ** (1 / 2)) / 2 * one;
+	let arrayX = [firstX, secondX];
+
+   	if (D < 0) {
+   	 arrayX = "Уравнение имеет только мнимые корни";
+ 	} else if (one == 0 && two == 0 && three == 0) {
+	  arrayX = "{-∞ ; +∞}";
+	} else if (D == 0) {
+       arrayX = (-two) / 2 * one;	 
+	} else {
+	    return arrayX;
+	}
+return arrayX;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+	"use strict";
+
+    let appraisals = marks;
+    let arrayMarks = [appraisals];
+	let result = (arrayMarks[0] + arrayMarks[1] + arrayMarks[2] + arrayMarks[3] + arrayMarks[4]) / 5;
+
+	if (arrayMarks.length == 0) {
+		result = "0";
+	} else if (arrayMarks.length > 5) {
+		 let arrayMarksTwo = arrayMarks.slice(0, 5);
+		 console.log(marks);
+		 return result;
+	} else {
+		  return result;
+	}
+
+return result;
 }
 
 function askDrink(name,dateOfBirthday){
+	"use strict";
+
     // код для задачи №3 писать здесь
     // return result;
 }
