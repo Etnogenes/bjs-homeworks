17ca489c63f073ba0d00afe614696ade43dc8aa9
Etnogenes <andrei09.94@mail.ru>
2020-12-26 16:32:49 +0300

    will be updated

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,9 +1,34 @@
+"use strict";
+/*
+let text = {
+	string: "palindrome",
+} ;
+
+function isPalindrome() {
+
+}
+isPalindrome.prototype = text;
+
 //String.prototype.isPalindrome - для задачи №1
 
+*/
+
+
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+
+	let arrayMarks = [marks];
+	let average = 0;
+	for (let i=0; i < arrayMarks.length; i++) {
+	 average += arrayMarks[i] / arrayMarks.length;
+	}
+	
+	let roundedAverage = Math.round(average);
+
+return roundedAverage;
 }
+
+
+
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
