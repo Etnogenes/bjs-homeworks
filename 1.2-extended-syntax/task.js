d167adef9ff05570cb2daf36a7fc1233b4021368
Etnogenes <andrei09.94@mail.ru>
2020-12-29 19:37:55 +0300

    I'm tired

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -2,53 +2,48 @@
 
 function getResult(a,b,c){
 
-    let D = b ** 2 - 4 * a * c;
+	let arrayX = [];
+	let D = b ** 2 - 4 * a * c;
 
-   	if (D < 0) {
-   	 arrayX = [];
-	} else if (D == 0) {
-       arrayX = [(-b) / 2 * a];	 
-	} else {
-	    return arrayX;
-	}
-
-    let firstX = (-b + Math.sqrt(D)) / 2 * a;
-    let secondX = (-b - Math.sqrt(D)) / 2 * a;
-	let arrayX = [firstX, secondX];
+   	if (D == 0) {
+   		arrayX = [-b / 2 * a];
+   	}
+   	if (D > 0) {
+   		arrayX = [(-b + Math.sqrt(D)) / 2 * a, (-b - Math.sqrt(D)) / 2 * a];
+   	}
 
 return arrayX;
 }
 
 function getAverageMark(marks){
    
-    	let result = 0;
+    let result = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+    	let sumOne += marks[i];
+    }
 
 	if (marks.length == 0) {
-		return result;
+		result = 0;
 	} else if (marks.length > 5) {
 		 let marksTwo = marks.slice(0, 5);
-	} else (marks.length <= 5) {
-		  for (let i = 0; i < marks.length; i++) {
-		    result += marks[i] / marks.length;
-		}
+		 result = marksTwo.reduce(function(sum, current) {
+		 	return sum + current
+		 }) / 5;
+	} else {
+		  result = sumOne / marks.length;
 	}
+	
 
 return result;
 }
 
 function askDrink(name,dateOfBirthday){
 
-	let dataNow = new Date();
-	let yearNow = dataNow.getFullYear();	
-	let ageVisitor = dateOfBirthday.getFullYear();
-	let result = yearNow - ageVisitor;
-
-		if (result >= 18) {
-		result = `Не желаете ли олд-фэшн, ${name}?`;
+		if ((new Date().getFullYear() - dateOfBirthday.getFullYear()) >= 18) {
+		return (`Не желаете ли олд-фэшн, ${name}?`);
 		} else {
-		 result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
+		 return (`Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`);
 		}
-
-return result;
 }
 
