25265c1602b05eb41e6f39dc14364669590fb5e3
Etnogenes <andrei09.94@mail.ru>
2021-02-13 14:09:49 +0300

    Must be right

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -3,26 +3,14 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     
 	let totalMonth = date.getMonth() - new Date.getMonth();
-
 	let bodyCredit = amount - contribution;
-
 	let monthPay = bodyCredit * ((percent / 12) + (percent / 12) / (((1 + (percent / 12)) ** totalMonth) - 1)) ;
-
 	let totalSum = Math.floor(monthPay * totalMonth * 100) / 100; // Я специально избегаю toFixed()
 
     return totalSum;
 }
 
 
-
-
-
-
-
-
 function getGreeting(name) {
-    
-    if (!name) {
-    	return (`Привет, мир! Меня зовут ${name || "Аноним"}.`);
-    }
+    return (`Привет, мир! Меня зовут ${name || "Аноним"}.`);
 }
