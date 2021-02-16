efff7dd9e0872ac52e59086659e2a5a0c08294ef
Etnogenes <andrei09.94@mail.ru>
2021-02-16 13:53:19 +0300

    Finaly...

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -19,17 +19,14 @@ function getAverageMark(marks){
    
     let result = 0;
 
-
 	if (marks.length == 0) {
-		result = 0;
-	} else if (marks.length > 5) {
-		 marks = marks.slice(0, 5);
-	} else {
-		  for (let i = 0; i < marks.length; i++) {
-		  	let result += marks[i] / marks.length;
-		  }
+		return result;
 	}
-	
+
+	marks = marks.slice(0, 5);
+	 for (let i = 0; i < marks.length; i++) {
+		  	result += marks[i] / marks.length;
+		  }
 
 return result;
 }
@@ -37,7 +34,7 @@ return result;
 function askDrink(name,dateOfBirthday){
 
 		if ((new Date().getFullYear() - dateOfBirthday.getFullYear()) >= 18) {
-		return (`Не желаете ли олд-фэшн, ${name}?`);
+		return (`Не желаете ли олд-фэшн, ${name || "Аноним"}?`);
 		} else {
 		 return (`Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`);
 		}
